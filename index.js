03e6ec06549ac1c6d71760c49b383cdc9f955a8a
Rstreamingent <160311118+Rstreamingent@users.noreply.github.com>
2024-02-27 13:17:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ const client = new MongoClient(uri, {
 });
 
 app.get('/', async (req, res) => {
+ res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
   try {
     await client.connect();
     console.log('Connected to the database');
